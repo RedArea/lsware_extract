b785e20ee8e126be64f9fecd2ba9d99a77e64aaf
RedArea <minsu2439@naver.com>
2020-10-27 16:27:25 +0900

    fix : SyntaxError: invalid syntax, TypeError: coercing to Unicode: need string or buffer, file found

--- lsware_extract/__init__.py
+++ lsware_extract/__init__.py
@@ -5,15 +5,16 @@ import argparse
 def main(argv=None):
     parser = argparse.ArgumentParser(description="lsware_extract dummy tool")
     parser.add_argument('-type', help="select type [all, part, partial]", required=True, choices=['all', 'part', 'partial'])
-    parser.add_argument('-in', help="input the path of the content file", required=True)
+    parser.add_argument('-in', help="input the path of the content file", required=True, dest='content')
     parser.add_argument('-media', help="select media [image, video, audio, mobile]", required=True, choices=['image', 'video', 'audio', 'mobile'])
-    parser.add_argument('-log', help="input the path of the log file", required=True, type=argparse.FileType('w'))
+    parser.add_argument('-log', help="input the path of the log file", required=True, type=argparse.FileType('a'))
     parser.add_argument('-out', help="input the path of the dna file")
 
     args = parser.parse_args()
-    
-    with open(args.log, "w") as f:
-        f.write(args.type)
-        f.write(args.in)
-        f.write(args.media)
-        f.write(args.log)
+    args.log.close()
+
+    with open(args.log.name, 'a') as f:
+        f.write(args.type + '\n')
+        f.write(args.content + '\n')
+        f.write(args.media + '\n')
+        f.write(args.log.name + '\n')
