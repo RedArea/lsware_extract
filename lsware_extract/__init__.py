dcd9ab33453b5e2fd2aca9af3d6032ec15864bd5
Mark <minsu2439@naver.com>
2020-10-29 18:41:02 +0900

    mod : miss sorry

--- lsware_extract/__init__.py
+++ lsware_extract/__init__.py
@@ -9,7 +9,7 @@ def _real_main(argv=None):
     parser.add_argument('-type') #, help="select type [all, part, partial]", required=True, choices=['all', 'part', 'partial'])
     parser.add_argument('-in', dest='content') #, help="input the path of the content file", required=True)
     parser.add_argument('-media') #, help="select media [image, video, audio, mobile]", required=True, choices=['image', 'video', 'audio', 'mobile'])
-    parser.add_argument('-log, help="input the path of the log file", required=True, type=argparse.FileType('a'))
+    parser.add_argument('-log', help="input the path of the log file", required=True, type=argparse.FileType('a'))
     parser.add_argument('-out') #, help="input the path of the dna file")
     args = parser.parse_args()
     
