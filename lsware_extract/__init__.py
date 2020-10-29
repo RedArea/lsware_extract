b027a7ad4ccf3e86ae445e9ecca832d5baf4089b
Mark <minsu2439@naver.com>
2020-10-29 18:15:01 +0900

    mod : part 기능

--- lsware_extract/__init__.py
+++ lsware_extract/__init__.py
@@ -6,11 +6,11 @@ from random import randint
 
 def _real_main(argv=None):
     parser = argparse.ArgumentParser(description="lsware_extract dummy tool")
-    parser.add_argument('-type' , default=None) #, help="select type [all, part, partial]", required=True, choices=['all', 'part', 'partial'])
-    parser.add_argument('-in'   , default=None, dest='content') #, help="input the path of the content file", required=True)
-    parser.add_argument('-media', default=None) #, help="select media [image, video, audio, mobile]", required=True, choices=['image', 'video', 'audio', 'mobile'])
-    parser.add_argument('-log'  , default=None, help="input the path of the log file", required=True, type=argparse.FileType('a'))
-    parser.add_argument('-out'  , default=None) #, help="input the path of the dna file")
+    parser.add_argument('-type' , default='') #, help="select type [all, part, partial]", required=True, choices=['all', 'part', 'partial'])
+    parser.add_argument('-in'   , default='', dest='content') #, help="input the path of the content file", required=True)
+    parser.add_argument('-media', default='') #, help="select media [image, video, audio, mobile]", required=True, choices=['image', 'video', 'audio', 'mobile'])
+    parser.add_argument('-log'  , default='', help="input the path of the log file", required=True, type=argparse.FileType('a'))
+    parser.add_argument('-out'  , default='') #, help="input the path of the dna file")
     args = parser.parse_args()
     
     type    = args.type.lower()
@@ -18,17 +18,24 @@ def _real_main(argv=None):
     media   = args.media.lower()
     log     = args.log
     out     = args.out
+    errmsg  = ""
 
     start_time = time.strftime('%Y-%m-%d %H:%M:%S')
     sleep(randint(0, 3))
 
-    if type is None or content is None:
-        log.write()## 오류 로그 포함 작성 후 종료
+    if not type or not content:
+        end_time = time.strftime('%Y-%m-%d %H:%M:%S')
+        errmsg = "parameter error : {type}, {content}, {media}, {log}, {out}".format(type=type, content=content, media=media, log=log, out=out)
+        log_msg = "{start_time}, {content}, {end_time}, {errmsg}".format(start_time=start_time, content=content, end_time=end_time, errmsg=errmsg)
+        log.write(log_msg)
         sys.exit()
 
     if type is 'all':
-        if out is None:
-            log.write()## 오류 로그 포함 작성 후 종료
+        if not out:
+            end_time = time.strftime('%Y-%m-%d %H:%M:%S')
+            errmsg = "parmeter error : Missing path to save feature point file"
+            log_msg = "{start_time}, {content}, {end_time}, {errmsg}".format(start_time=start_time, content=content, end_time=end_time, errmsg=errmsg)
+            log.write(log_msg)
             sys.exit()
         else:
             if out[-1] is not '/':
@@ -65,10 +72,33 @@ def _real_main(argv=None):
                     if not line: break
                     vdna.write(line)   
         
-        log.write()## 로그 작성 후 종료
+        end_time = time.strftime('%Y-%m-%d %H:%M:%S')
+        log_msg = "{start_time}, {content}, {end_time}, {errmsg}".format(start_time=start_time, content=content, end_time=end_time, errmsg=errmsg)
+        log.write(log_msg)
         sys.exit()
     else:
-        print("part")
+        find_value = ""
+        random_num = randint(0, 10)
+
+        if random_num is not 0 or random_num is not 9:
+            if random_num < 3:
+                find_value = 1
+            else:
+                find_value = randint(0,100)
+
+        elif random_num is 9:
+            errmsg = "error : random number is 9" 
+
+        query_time = time.strftime('%Y-%m-%d %H:%M:%S')
+        end_time = time.strftime('%Y-%m-%d %H:%M:%S')
+    
+        if not errmsg:   
+            log_msg = "{start_time}, {content}, {query_time}, {find_value}, {end_time}".format(start_time=start_time, content=content, query_time=query_time, find_value=find_value, end_time=end_time)
+        else:
+            log_msg = "{start_time}, {content}, {query_time}, {find_value}, {end_time}, {errmsg}".format(start_time=start_time, content=content, query_time=query_time, find_value=find_value, end_time=end_time, errmsg=errmsg)
+
+        log.write(log_msg)
+        sys.exit()
 
 def main(argv=None):
     try:
@@ -76,7 +106,7 @@ def main(argv=None):
     except OSError as e:
         print "Invalid log file path.", e
     except Exception as e:
-        print "exception!!!!!!!!!!", e
+        print "unhandled exception!", e
 
 
  
